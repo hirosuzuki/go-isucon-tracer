ed8c429007f820fbab5df3457c62c5dbe9ff505e
Hiro Suzuki <hiro.suzuki.oct10@gmail.com>
2020-08-22 13:52:45 +0000

    first version

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/hirosuzuki/go-isucon-tracer
 
 go 1.14
 
-require github.com/shogo82148/go-sql-proxy v0.3.0
+require (
+	github.com/pkg/profile v1.5.0
+	github.com/shogo82148/go-sql-proxy v0.3.0
+)
--- tracer.go
+++ tracer.go
@@ -7,19 +7,75 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"path"
 	"regexp"
 	"strings"
+	"syscall"
 	"time"
 
+	"github.com/pkg/profile"
 	proxy "github.com/shogo82148/go-sql-proxy"
 )
 
-// SQLLoggerInit ... Register SQL Logger Drivers
-func SQLLoggerInit(logFileName string) {
+var traceID string
+var sqlLogFileName string
+var sqlLogFile *os.File
+var perfomanceLogFileName string
+var perfomanceLogFile *os.File
+var profilerHandle interface{ Stop() }
 
-	logger, err := os.Create(logFileName)
-	if err != nil {
+// Initialize ISUCON Tracer
+// Wait signal (HUP, INT, TERM, QUIT)
+func Initialize() {
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		signal := <-signalCh
+		log.Printf("ISUCON Tracer Catch Signal (%s)\n", signal)
+		if signal == syscall.SIGHUP {
+			Stop()
+			Start()
+		} else {
+			Stop()
+			os.Exit(0)
+		}
+	}()
+	Start()
+}
+
+// Start ISUCON Tracer Start
+func Start() {
+
+	var err error
+
+	if traceID != "" {
+		Stop()
+	}
+
+	traceID = time.Now().Format("isucon-20060102-150405")
+	log.Printf("ISUCON Tracer Start (%s)\n", traceID)
+
+	// Base Log Directory
+	logDirName := path.Join("/tmp/isucon/", traceID)
+	if err = os.MkdirAll(logDirName, 0755); err != nil {
 		log.Fatal(err)
+	}
+
+	profilerHandle = profile.Start(profile.ProfilePath(logDirName), profile.NoShutdownHook)
+
+	// Create SQL Log File
+	sqlLogFileName = path.Join(logDirName, "sql.log")
+	if sqlLogFile, err = os.Create(sqlLogFileName); err != nil {
+		log.Printf("ISUCON Tracer Error: %s\n", err.Error())
+		return
+	}
+
+	// Create Perfomance Log File
+	perfomanceLogFileName = path.Join(logDirName, "perfomance.log")
+	if perfomanceLogFile, err = os.Create(sqlLogFileName); err != nil {
+		log.Printf("ISUCON Tracer Error: %s\n", err.Error())
+		return
 	}
 
 	rep := regexp.MustCompile(`[ \n\t]{2,}`)
@@ -32,8 +88,9 @@ func SQLLoggerInit(logFileName string) {
 		startTime := ctx.(int64)
 		timeDelta := now.UnixNano() - startTime
 		query := rep.ReplaceAllString(stmt.QueryString, " ")
-		record := fmt.Sprintf("%d\t%d\t%s", startTime, timeDelta, query)
-		fmt.Fprintln(logger, record)
+		if sqlLogFile != nil {
+			fmt.Fprintf(sqlLogFile, "%d\t%d\t%s", startTime, timeDelta, query)
+		}
 		return nil
 	}
 
@@ -44,7 +101,7 @@ func SQLLoggerInit(logFileName string) {
 		db, _ := sql.Open(driverName, "")
 		defer db.Close()
 		newDriverName := driverName + ":logger"
-		log.Printf("Register: %s\n", newDriverName)
+		log.Printf("ISUCON Tracer SQL Driver Register: %s\n", newDriverName)
 		sql.Register(driverName+":logger", proxy.NewProxyContext(db.Driver(), &proxy.HooksContext{
 			PreExec: PreFunc,
 			PostExec: func(c context.Context, ctx interface{}, stmt *proxy.Stmt, args []driver.NamedValue, result driver.Result, err error) error {
@@ -55,5 +112,23 @@ func SQLLoggerInit(logFileName string) {
 				return PostFunc(c, ctx, stmt, args, err)
 			},
 		}))
+	}
+
+}
+
+// Stop ISUCON Tracer Stop
+func Stop() {
+	if traceID != "" {
+		traceID = ""
+		log.Printf("ISUCON Tracer End (%s)\n", traceID)
+	}
+	if profilerHandle != nil {
+		profilerHandle.Stop()
+	}
+	if sqlLogFile != nil {
+		sqlLogFile.Close()
+	}
+	if perfomanceLogFile != nil {
+		perfomanceLogFile.Close()
 	}
 }
